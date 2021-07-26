2aac5dc6298e72b390a44be5facdd3030a16e3f1
NicolasLegendre1 <nicolaslegendre963@yahoo.com>
2021-07-27 00:14:16 +0200

    Generative rotated molecules from a finite set of particules

--- sim_volumes.py
+++ sim_volumes.py
@@ -1,6 +1,7 @@
 """Generate 3D map of molecules."""
 
 import numpy as np
+import os
 from geomstats.geometry.special_orthogonal import SpecialOrthogonal
 
 SO3 = SpecialOrthogonal(n=3, point_type="vector")
@@ -12,22 +13,30 @@ NAME = "4_points_3d"
 CENTER = 2
 
 
-def get_random_quat(num_pts):
+def uniform_quaternion(num_pts):
     """Generate a list of quaternions by using uniform distribution.
 
     Parameters
     ----------
     num_pts : int
-        number of quaternions to return.
+        Number of quaternions to return.
 
     Returns
     -------
     array
-        list of simulated quaternions of shape [num_pts,4].
+        List of simulated quaternions of shape [num_pts,4].
+
+    Source
+    -------
+    Graphics Gems III (IBM Version)
+    III.6 - UNIFORM RANDOM ROTATIONS
+    David Kirk
+         https://www.sciencedirect.com/science/article/pii/B9780080507552500361
+    http://planning.cs.uiuc.edu/node198.html
+
 
     """
-    u = np.random.rand(3, num_pts)
-    u1, u2, u3 = [u[x] for x in range(3)]
+    u1, u2, u3 = np.random.rand(3, num_pts)
 
     quat = np.zeros((4, num_pts))
     quat[0] = np.sqrt(1 - u1) * np.sin(np.pi * u2 * 2)
@@ -44,40 +53,42 @@ def uniform_rotations(num_pts):
     Parameters
     ----------
     num : int
-        number of quaternion to return.
+        Number of quaternion to return.
 
     Returns
     -------
     rots : ndarray
-        matrix of rotation of the simulated quaternions of shape (num_pts,3,3)
+        Matrix of rotation of the simulated quaternions of shape (num_pts,3,3).
     qs : array
-        list of simulated quaternions of shape [num_pts,4].
+        List of simulated quaternions of shape [num_pts,4].
 
     """
-    qs = get_random_quat(num_pts)
-    rots = SO3.matrix_from_quaternion(qs)  # num,3,3
+    qs = uniform_quaternion(num_pts)
+    rots = SO3.matrix_from_quaternion(qs)
     return (rots, qs)
 
 
 def modify_weight(points, volume, vol_size, center):
     """
-    Fill an empty volume with particles.
+    Fill an empty volume with particles. The volume is represented as an empty
+    voxel. each atome of the molecule is localized around a point with a
+    gaussian probability of presence.
 
     Parameters
     ----------
     points : array
-        list particules position.
+        List particules position.
     volume : ndarray
-        volume of the molecule.
+        Volume of the molecule.
     size : int
-        size of the volume.
+        Size of the volume.
     center : int
-        center the molecule around zero.
+        Center the molecule around zero.
 
     Returns
     -------
     volume : ndarray
-        volume of the molecule.
+        Volume of the molecule.
 
     """
     for point in points.T:
@@ -98,20 +109,20 @@ def simulate_volumes(particules, n_volumes, vol_size, center=2):
     Parameters
     ----------
     particules : array
-        list particules position.
+        List particules position.
     n_volumes : int
-        number of data.
+        Number of data.
     img_size : int
-        size of the molecule.
+        Size of the molecule.
     center : int
-        optional center the molecule around zero.
+        Optional center the molecule around zero.
 
     Returns
     -------
     volumes : ndarray
         3D map of the molecule.
     qs : list
-        describes rotations in quaternions.
+        Describes rotations in quaternions.
 
     """
     rots, qs = uniform_rotations(n_volumes)
@@ -121,7 +132,6 @@ def simulate_volumes(particules, n_volumes, vol_size, center=2):
             print(idx)
         points = rots[idx].dot(particules)
         volumes[idx] = modify_weight(points, volumes[idx], vol_size, center)
-    qs = [np.array2string(q) for q in qs]
     return volumes, qs
 
 
@@ -132,34 +142,36 @@ def save_volume(particules, n_volumes, vol_size, main_dir, name, center=2):
     Parameters
     ----------
     particules : array
-        position of particules.
+        Position of particules.
     n_volumes : int
-        number of data.
+        Number of data.
     vol_size : int
-        size of the volume.
+        Size of the volume.
     main_dir : string
-        main directory.
+        Main directory.
     name : string
-        name.
+        Name.
     center : int
-        center the molecule around 0.
+        Center the molecule around 0.
 
     Returns
     -------
     volumes : ndarray
         3D map of the molecule
     labels : ndarray
-        describes rotations in quaternions
+        Describes rotations in quaternions
 
     Example
     -------
     >>>import numpy as np
     >>>import coords
-    _ = save_volume(PARTICULES,N_VOLUMES,VOL_SIZE, dir, NAME, CENTER)
+    >>> _ = save_volume(PARTICULES, N_VOLUMES, VOL_SIZE, dir, NAME, CENTER)
     """
+    path_molecules = os.path.join(main_dir, name + "_molecules.npy")
+    path_labels = os.path.join(main_dir, name + "_labels.npy")
     volumes, labels = simulate_volumes(
         particules, n_volumes, vol_size, center)
-    np.save(main_dir + name + '_molecules.npy', volumes)
-    np.save(main_dir + name + '_labels.npy', volumes)
+    np.save(path_molecules, volumes)
+    np.save(path_labels, volumes)
 
     return volumes, labels
