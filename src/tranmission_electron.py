b615155ea4e3fdd1b89ed2b944b8538f54acd4fb
arjunsingh3600 <swaniarjun@gmail.com>
2021-10-23 00:08:12 -0700

    Cleaned up. added class structure

--- src/tranmission_electron.py
+++ src/tranmission_electron.py
@@ -1,5 +1,4 @@
 import os
-import numpy as np
 from TEM_utils.cryoemio import simio, mrc2data
 from TEM_utils.simutils import define_grid_in_fov,write_crd_file,fill_parameters_dictionary,write_inp_file
 from pathlib import Path
@@ -10,111 +9,130 @@ SIMULATOR='../Material/tem-Simulator/TEM-simulator/Release/TEM-simulator.exe'
 PDBFILE = './temp_workspace/input/4v6x.pdb'
 CONFIG = './temp_workspace/input/configurations.yaml'
 
-def getConfigFromYaml(configFile):
-    #
-    # Parses Yaml file and returns the following : seed,sample_dimensions,beam_params,optics_params,detector_params
-    # ( seed is a random int)
-    #
-    # get configurations from yaml.
-    # For now, yaml must specify all configurations in the notebook in this format (configurations.yaml)
-    #
-    # Questions:
-    # - How do people usually format their YAMLS? Is it in arrays or key:value for each parameter?
-    # - If there is no standard YAML config format, do we specify? or do we support all sorts of config files?
-    # TODO:
-    # - Have default configurations, overwrite from YAML
-    # - update YAML parsing based on answers to questions above (rn kinda scuffed)
 
+# Have more verbose comments rn instead of docstrings
 
-    with open(configFile, 'r') as stream:
-        parsedConfig = yaml.load(stream)
-    parsedConfig['seed'] = 1234;
+class TEMSimulatorHelper:
 
-    return list(parsedConfig.values())
-
-def getIOFilePaths(pdbFile):
+# Most static functions are wrapper functions that can accept dictionaries for arguments.
+# Easier than dealing with a bunch of args
 #
-# return list of file paths in order [ pdb_file, mrc_file, crd_file, log_file, inp_file ]
-#
-# trying to create output dir next to input file.
-# Not working. paths needing fixing.
+# TODO
+#   - add functionality to change configuration
+#   - What should nameing convention be? camel case or underscores
+#   - Verify functionality
+#   - Establish compatibility for later python versions
+#   - Figure out simulator wrapper
 
-    if not os.path.isfile(pdbFile):
-        raise Exception("Error opening File")
+    def __init__(self,configFile):
+        # Seperated constructor and running actual sim -> lets you run a  bunch of input files with same configs
+        # not sure if the helper methods should be made static or not
 
-    # suffix = str(datetime.now().strftime("%m-%d-%Y %H %M %S"))
-    suffix = ''
-    inputFile = pdbFile
-    inputDir = os.path.dirname(pdbFile) + '/'
-    inputName = os.path.splitext(os.path.basename(inputFile))[0]
+        self.configurations = self._getConfigFromYaml(configFile)
+        self.filePaths = {}
+        self.paramDict = {}
 
-    outDir = inputDir + f"{inputName} simulation output {suffix}/"
+    def runSim(self,pdbFile):
 
-    # if not os.path.exists(outDir):
-    #     os.makedirs(Path(outDir))
+        self.filePaths = self._getIOFilePaths(pdbFile)
+        self._buildCordFile(**self.filePaths,**self.configurations)
+        self.paramDict = self._buildParamDict(**self.filePaths,**self.configurations)
+        self._buildInpFile(self.paramDict,**self.filePaths)
 
-    return simio(inputDir,inputName, outDir, inputName)
+        data = self._getImageData(**self.filePaths)
+        return data
 
-def buildCordFile(crd_file,pdb_file,sample_dimensions, optics_params, detector_params):
-    # use simutils to build co-ordinate file
-    x_range, y_range, numpart = define_grid_in_fov(sample_dimensions,
-                                                            optics_params,
-                                                            detector_params,
-                                                            pdb_file=pdb_file,
-                                                            Dmax=30, pad=5.)
+    @staticmethod
+    def _getConfigFromYaml(configFile):
+        #
+        # Parses Yaml file and returns the following : seed,sample_dimensions,beam_params,optics_params,detector_params
+        # ( seed is a random int)
+        #
+        # get configurations from yaml.
+        # For now, yaml must specify all configurations in the notebook in this format (configurations.yaml)
+        #
+        # Questions:
+        # - How do people usually format their YAMLS? Is it in arrays or key:value for each parameter?
+        # - If there is no standard YAML config format, do we specify? or do we support all sorts of config files?
+        # TODO:
+        # - Have default configurations, overwrite from YAML
+        # - update YAML parsing based on answers to questions above (rn kinda scuffed)
 
-    write_crd_file(numpart, xrange=x_range, yrange=y_range, crd_file=crd_file)
+        with open(configFile, 'r') as stream:
+            parsedConfig = yaml.load(stream)
+        parsedConfig['seed'] = 1234;
 
-def runSimulator(inp_file,mrc_file):
-    # generate command and run simulator, return mrcData
-    SIMULATOR_BIN = Path(SIMULATOR) # might have to change depending on OS
-    inp_file = Path(inp_file)
+        return parsedConfig
 
-    cmd = '{0} {1}'.format(SIMULATOR_BIN, inp_file)
-    os.system(cmd)
+    @staticmethod
+    def _getIOFilePaths(pdbFile):
+        #
+        # return dict of file paths with keys [ pdb_file, mrc_file, crd_file, log_file, inp_file,h5_file ]
+        #
+        # Use the input file to create ouput dir in same location
+        # possibly add an output dir?
+        # Implement
 
-    return mrc2data(mrc_file = mrc_file)
+        filePaths = {}
+        return filePaths
+
+    @staticmethod
+    def _buildCordFile(crd_file, pdb_file, sample_dimensions, optics_params, detector_params,**kwargs):
+        # use simutils to build co-ordinate file
+        # Creates .crd file in the crd_file path
+        x_range, y_range, numpart = define_grid_in_fov(sample_dimensions,
+                                                       optics_params,
+                                                       detector_params,
+                                                       pdb_file=pdb_file,
+                                                       Dmax=30, pad=5.)
+
+        write_crd_file(numpart, xrange=x_range, yrange=y_range, crd_file=crd_file)
+
+    @staticmethod
+    def _getImageData(inp_file, mrc_file,**kwargs):
+        # generate command and run simulator, return mrcData
+        # create mrc file
+
+        SIMULATOR_BIN = Path(SIMULATOR)  # might have to change depending on OS
+        inp_file = Path(inp_file)
+
+        cmd = '{0} {1}'.format(SIMULATOR_BIN, inp_file)
+        os.system(cmd)
+
+        return mrc2data(mrc_file=mrc_file)
+    @staticmethod
+    def _buildParamDict(mrcFile,pdbFile,crdFile,sampleDimensions,beamParams,opticsParams,detectorParams,logFile,seed,**kwargs):
+        return fill_parameters_dictionary(mrc_file=mrcFile,
+                                                       pdb_file=pdbFile,
+                                                       particle_mrcout=mrcFile,
+                                                       crd_file=crdFile,
+                                                       sample_dimensions=sampleDimensions,
+                                                       beam_params=beamParams,
+                                                       optics_params=opticsParams,
+                                                       detector_params=detectorParams,
+                                                       log_file=logFile,
+                                                       seed=seed)
+
+    @staticmethod
+    def _buildInpFile(paramDict,inpFile,**kwargs):
+        # mutates inpFile
+        write_inp_file(inp_file=inpFile, dict_params=paramDict)
 
 
 def TEMSimulator(configFile, pdbFile):
+#
+# for user, allows running sim in one step.
+#
+# TODO:
+# - add flags for keeping generate files and mrc file
+# - Final output should be particle stack with metadata
 
-    # Using more verbose comments instead of docstring for notes (for now)
-    #
-    #
-    # Arguments -> configFile, pdbFile
-    #
-    # Target Output -> A particle stack with meta data (maybe look into .hdf5 )
 
-
-    beam_params,detector_params,optics_params,sample_dimensions,seed  = getConfigFromYaml(configFile)
-    pdb_file, mrc_file, crd_file, log_file, inp_file,h5_file = getIOFilePaths(pdbFile)
-    buildCordFile(crd_file,pdb_file,sample_dimensions, optics_params, detector_params )
-    #
-    params_dictionary = fill_parameters_dictionary(mrc_file = mrc_file,
-                                                        pdb_file = pdb_file,
-                                                        particle_mrcout = mrc_file,
-                                                        crd_file = crd_file,
-                                                        sample_dimensions = sample_dimensions,
-                                                        beam_params = beam_params,
-                                                        optics_params = optics_params,
-                                                        detector_params = detector_params,
-                                                        log_file = log_file,
-                                                        seed=1234)
-
-    write_inp_file(inp_file=inp_file, dict_params=params_dictionary)
-
-    mrc_data = runSimulator(inp_file,mrc_file)
-    print(mrc_data.shape)
-
-    return
+    simHelper = TEMSimulatorHelper(configFile,pdbFile)
+    mrc_data = simHelper.runSim()
 
 def main():
-    # General Notes
-    # unpacking/packing arguments feels kinda complicated and prone to error.
-    #TEMSimulatr() - main function. rest are helpers
-
-    TEMSimulator(CONFIG,PDBFILE)
-    # print(getIOFilePaths(PDBFILE))
+    data = TEMSimulator(CONFIG,PDBFILE)
 
 if __name__ == "__main__":
     main()
