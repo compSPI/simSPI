a4e75976a5f8364ab90da95f657b68fc00baf048
Geoffrey Woollard <geoffwoollard@gmail.com>
2021-11-05 15:25:27 -0700

    rename N to num_

--- tests/test_multislice.py
+++ tests/test_multislice.py
@@ -9,11 +9,11 @@ from simSPI import multislice, transfer
 
 def test_exit_wave_to_image():
     """High dose, no ctf/dqe/ntf."""
-    N = 64
-    sphere = raster_geometry.sphere([N, N, N], radius=N // 8, position=0.25).astype(
-        np.float32
-    )
-    ones = np.ones((N, N))
+    num_pix = 64
+    sphere = raster_geometry.sphere(
+        [num_pix, num_pix, num_pix], radius=num_pix // 8, position=0.25
+    ).astype(np.float32)
+    ones = np.ones((num_pix, num_pix))
     exit_wave = sphere.sum(-1)
     exit_wave_f = fourier.do_fft(exit_wave, dim=2)
     high_dose = 1e9 * exit_wave.max()
@@ -27,24 +27,24 @@ def test_exit_wave_to_image():
         ntf=ones,
     )
 
-    assert i.shape == (N, N)
-    assert shot_noise_sample.shape == (N, N)
-    assert i0_dqe.shape == (N, N)
-    assert i0.shape == (N, N)
+    assert i.shape == (num_pix, num_pix)
+    assert shot_noise_sample.shape == (num_pix, num_pix)
+    assert i0_dqe.shape == (num_pix, num_pix)
+    assert i0.shape == (num_pix, num_pix)
     assert np.allclose(i / high_dose, exit_wave, atol=1e-4)
 
 
 def test_apply_poisson_shot_noise_sample():
     """Poisson noise high vs low."""
-    N = 64
-    signal = np.ones((N, N))
+    num_pix = 64
+    signal = np.ones((num_pix, num_pix))
     # hi noise
     dose_hin = 0.1
     noise_bg_hin = 1
     shot_noise_sample_hin = multislice.apply_poisson_shot_noise_sample(
         signal=signal, dose=dose_hin, noise_bg=noise_bg_hin
     )
-    assert shot_noise_sample_hin.shape == (N, N)
+    assert shot_noise_sample_hin.shape == (num_pix, num_pix)
 
     # hi dose
     dose_hid = 1
@@ -64,37 +64,37 @@ def test_apply_poisson_shot_noise_sample():
 
 def test_apply_complex_ctf_to_exit_wave():
     """Test apply_complex_ctf_to_exit_wave."""
-    N_random = np.random.uniform(low=50, high=100)
-    N = int(2 * (N_random // 2))  # even N
-    assert np.isclose(N % 2, 0), "must be even for test to work"
+    num_random = np.random.uniform(low=50, high=100)
+    num_pix = int(2 * (num_random // 2))  # even N
+    assert np.isclose(num_pix % 2, 0), "must be even for test to work"
     i0 = multislice.apply_complex_ctf_to_exit_wave(
-        exit_wave_f=np.ones((N, N)), complex_ctf=np.ones((N, N))
+        exit_wave_f=np.ones((num_pix, num_pix)), complex_ctf=np.ones((num_pix, num_pix))
     )
-    assert i0.shape == (N, N)
+    assert i0.shape == (num_pix, num_pix)
 
 
 def test_apply_dqe():
     """Test apply_dqe."""
-    N_random = np.random.uniform(low=50, high=100)
-    N = int(2 * (N_random // 2))
-    assert np.isclose(N % 2, 0), "must be even for test to work"
-    ones = np.ones((N, N))
-    freq_A_2d = transfer.ctf_freqs(N, dim=2)[0]
+    num_random = np.random.uniform(low=50, high=100)
+    num_pix = int(2 * (num_random // 2))
+    assert np.isclose(num_pix % 2, 0), "must be even for test to work"
+    ones = np.ones((num_pix, num_pix))
+    freq_A_2d = transfer.ctf_freqs(num_pix, dim=2)[0]
     mtf_const = 1.5
     mtf2 = (np.sinc(freq_A_2d * mtf_const)) ** 2
     ntf2 = np.sinc(freq_A_2d) ** 2
     dqe = mtf2 / ntf2
     i0_dqe = multislice.apply_dqe(ones, dqe)
-    assert i0_dqe.shape == (N, N)
+    assert i0_dqe.shape == (num_pix, num_pix)
 
 
 def test_apply_ntf():
     """Test apply_ntf."""
-    N_random = np.random.uniform(low=50, high=100)
-    N = int(2 * (N_random // 2))
-    assert np.isclose(N % 2, 0), "must be even for test to work"
-    ones = np.ones((N, N))
-    freq_A_2d = transfer.ctf_freqs(N, dim=2)[0]
+    num_random = np.random.uniform(low=50, high=100)
+    num_pix = int(2 * (num_random // 2))
+    assert np.isclose(num_pix % 2, 0), "must be even for test to work"
+    ones = np.ones((num_pix, num_pix))
+    freq_A_2d = transfer.ctf_freqs(num_pix, dim=2)[0]
     ntf = np.sinc(freq_A_2d)
     i = multislice.apply_ntf(shot_noise_sample=ones, ntf=ntf)
-    assert i.shape == (N, N)
+    assert i.shape == (num_pix, num_pix)
