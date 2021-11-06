8eb3a89e6e5b587d90048f1cf1496e36bd81c135
thisTyler <t368@heim.ca>
2021-11-06 00:03:39 -0700

    Test changes

--- tests/test_tem.py
+++ tests/test_tem.py
@@ -19,7 +19,7 @@ def test_basic_sim():
     tmp_pdb = tempfile.NamedTemporaryFile(delete=False)
 
     try:
-        tmp_config.write(yaml.dump({'a': 0}))
+        tmp_config.write(yaml.dump({0: True}))
         sim = tem.TEMSimulator(
             tmp_paths.name,
             tmp_config.name,
