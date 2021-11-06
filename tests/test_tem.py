eb86e2b31b0824b38d3a5ee1d70fd785e19bc485
thisTyler <t368@heim.ca>
2021-11-05 22:18:41 -0700

    Fixed some formatting

--- tests/test_tem.py
+++ tests/test_tem.py
@@ -10,11 +10,11 @@ def test_tem_main():
 
 def test_basic_sim():
     """Tests simulator class initialization and basic functionality."""
-    simulator = tem.TEMSimulator('../test/paths.yml', '../test/sim.yml')
-    simulator.get_config_from_yaml('../test/config.yml')
-    tem.TEMSimulator.generate_path_dict('test.pdb')
+    simulator = tem.TEMSimulator("../test/paths.yml", "../test/sim.yml")
+    simulator.get_config_from_yaml("../test/config.yml")
+    tem.TEMSimulator.generate_path_dict("test.pdb")
 
-    simulator.run('test.pdb')
+    simulator.run("test.pdb")
 
     simulator.create_crd_file(0)
     simulator.get_image_data()
