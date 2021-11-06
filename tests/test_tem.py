41a96b6844340ca476bbdacd4c26235175edafd8
thisTyler <t368@heim.ca>
2021-11-05 22:01:56 -0700

    Added placeholder unit tests

--- tests/test_tem.py
+++ tests/test_tem.py
@@ -10,16 +10,11 @@ def test_tem_main():
 
 def test_basic_sim():
     """Tests simulator class initialization and basic functionality."""
-    paths_yaml = '../test/paths.yml'
-    sims_yaml = '../test/sim.yml'
-    config_yaml = '../test/config.yml'
-    test_pdb = 'test.pdb'
+    simulator = tem.TEMSimulator('../test/paths.yml', '../test/sim.yml')
+    simulator.get_config_from_yaml('../test/config.yml')
+    tem.TEMSimulator.generate_path_dict('test.pdb')
 
-    simulator = tem.TEMSimulator(paths_yaml, sims_yaml)
-    simulator.get_config_from_yaml(config_yaml)
-    tem.TEMSimulator.generate_path_dict(test_pdb)
-
-    simulator.run(test_pdb)
+    simulator.run('test.pdb')
 
     simulator.create_crd_file(0)
     simulator.get_image_data()
