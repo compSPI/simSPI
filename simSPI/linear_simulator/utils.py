911f5844b643c9fbb582d6cfd404158f655efc3e
Harshit Gupta <hrshit786@gmail.com>
2021-11-28 23:30:00 +0530

    precommit check utils.py

--- simSPI/linear_simulator/utils.py
+++ simSPI/linear_simulator/utils.py
@@ -1,29 +1,34 @@
+"""contain miscellaneous function."""
 import numpy as np
 import torch
 
 
 def U2T(x):
-    """converts samples from uniform distribution to triangular distribution
+    """Convert samples from uniform distribution to triangular distribution.
+
     Parameters
     ----------
-        x: torch.Tensor
+    x: torch.Tensor
+
     Returns
-    ---------
-        out: torch.Tensor
+    -------
+    out: torch.Tensor
     """
     return (x - 0.5).sign() * (1 - (1 - (2 * x - 1).abs()).sqrt())
 
 
 def primal_to_fourier_2D(r):
-    """returns fourier transform of a batch of image
+    """Return fourier transform of a batch of image.
+
     Parameters
     ----------
-        r: torch.Tensor
-            Tensor of size (batch,1, size,size)
+    r: torch.Tensor
+        Tensor of size (batch,1, size,size)
+
     Returns
-    ------
-        out: torch.Tensor
-            Tensor of size (batch,1, size,size)
+    -------
+    out: torch.Tensor
+        Tensor of size (batch,1, size,size)
     """
     r = torch.fft.fftshift(r, dim=(-2, -1))
     return torch.fft.ifftshift(
@@ -32,13 +37,15 @@ def primal_to_fourier_2D(r):
 
 
 def fourier_to_primal_2D(f):
-    """returns inverse fourier transform of a batch of image
+    """Return inverse fourier transform of a batch of image.
+
     Parameters
     ----------
     f: torch.Tensor
         Tensor of size (batch,1, size,size)
+
     Returns
-    ------
+    -------
     out: torch.Tensor
         Tensor of size (batch,1, size,size)
     """
@@ -49,47 +56,57 @@ def fourier_to_primal_2D(f):
 
 
 def init_cube(sidelen):
-    """Creates a volume with cube
+    """Create a volume with cube.
+
     Parameters
     ----------
-        sidelen: int
-            size of the volume
+    sidelen: int
+        size of the volume
+
     Returns
     -------
-        volume: torch.Tensor
-            Tensor (sidelen,sidelen,sidelen) with a cube of size (sidelen//2,sidelen//2,sidelen//2)
+    volume: torch.Tensor
+        Tensor (sidelen,sidelen,sidelen) with a cube
+        of size (sidelen//2,sidelen//2,sidelen//2)
     """
     L = sidelen // 2
-    l = sidelen // 8
+    length = sidelen // 8
     volume = torch.zeros([sidelen] * 3)
-    volume[L - l : L + l, L - l : L + l, L - l : L + l] = 1
+    volume[
+        L - length : L + length, L - length : L + length, L - length : L + length
+    ] = 1
     return volume
 
 
 class AttrDict(dict):
-    """class to convert a dictionary to a class
+    """Class to convert a dictionary to a class.
+
     Parameters
     ----------
-        dict: dictionary
+    dict: dictionary
+
     """
 
     def __init__(self, *args, **kwargs):
-        """Method to return a class with attributes equal to the input dictionry"""
+        """Return a class with attributes equal to the input dictionary."""
         super(AttrDict, self).__init__(*args, **kwargs)
         self.__dict__ = self
 
 
 def init_data(path):
-    """loads .npy file for the test functions from a path and converts its config dictionary into a class
+    """Load data for the test functions.
+
+    Loads .npy file from a path and converts its config dictionary into a class
+
     Parameters
     ----------
-        path: str
+    path: str
+
     Returns
     -------
-        saved_data: dict
-            loaded dictionary
-        config: object
-
+    saved_data: dict
+        loaded dictionary
+    config: object
     """
     saved_data = np.load(path, allow_pickle=True).item()
     if "config_dict" in saved_data:
@@ -101,14 +118,19 @@ def init_data(path):
 
 
 def normalized_mse(a, b):
-    """calculates mean square error between two inputs normalized by the norm of the first input
+    """Return mean square error.
+
+    Calclulates mean square error between two inputs normalized
+    by the norm of the first input.
+
     Parameters
     ----------
-        a: torch.Tensor
-        b: torch.Tensor
+    a: torch.Tensor
+    b: torch.Tensor
+
     Returns
     -------
-        out: torch.Tensor
-            normalized mse
+    out: torch.Tensor
+        normalized mse
     """
     return (a - b).pow(2).sum().sqrt() / a.pow(2).sum().sqrt()
