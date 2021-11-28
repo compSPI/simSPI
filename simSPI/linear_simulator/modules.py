e54ccf44be407e337eda75d916d66c04a5dc2945
Harshit Gupta <hrshit786@gmail.com>
2021-11-28 22:24:16 +0530

    precommit modules.py

--- simSPI/linear_simulator/modules.py
+++ simSPI/linear_simulator/modules.py
@@ -1,3 +1,4 @@
+"""contains simulator and its components classes."""
 import os
 
 import mrcfile
@@ -8,17 +9,22 @@ from pytorch3d.transforms import Rotate
 
 from .utils import fourier_to_primal_2D, init_cube, primal_to_fourier_2D
 
+"""Module to generate data using using liner forward model."""
+
 
 class simulator(torch.nn.Module):
+    """Class to generate data using liner forward model.
+
+    Parameters
+    ----------
+    config: class
+        Class containing parameters of the simulator
+
+    """
+
     def __init__(self, config, initial_volume=None):
         super(simulator, self).__init__()
-        """module that generates data using weak phase approximation (liner forward model) with a given config setting.
-            projector->ctf->shift->noise
-        Parameters
-        __________
-            config: class
-                Class containing parameters of the simulator
-        """
+
         self.config = config
         self.Projector = Projector(config)  # class for tomographic projector
         self.init_volume()  # changes the volume inside the projector
@@ -27,21 +33,22 @@ class simulator(torch.nn.Module):
         self.Noise = Noise(config)  # class for noise
 
     def forward(self, rot_params, ctf_params, shift_params):
-        """Creates cryoEM measurements using input parameters
+        """Create cryoEM measurements using input parameters.
+
         Parameters
-        __________
-            rot_params: dict of type str to {tensor}
-                Dictionary of rotation parameters for a projection chunk
-            ctf_params: dict of type str to {tensor}
-                Dictionary of Contrast Transfer Function (CTF) parameters for a projection chunk
-            shift_params: dict of type str to {tensor}
-                Dictionary of shift parameters for a projection chunk
+        ----------
+        rot_params: dict of type str to {tensor}
+            Dictionary of rotation parameters for a projection chunk
+        ctf_params: dict of type str to {tensor}
+            Dictionary of Contrast Transfer Function (CTF) parameters
+             for a projection chunk
+        shift_params: dict of type str to {tensor}
+            Dictionary of shift parameters for a projection chunk
 
         Returns
-        _______
-            projection.real : tensor
-                Tensor ([chunks,1,sidelen,sidelen]) contains cryoEM measurement
-
+        -------
+        projection.real : tensor
+            Tensor ([chunks,1,sidelen,sidelen]) contains cryoEM measurement
         """
         projection = self.Projector(rot_params)
         f_projection = primal_to_fourier_2D(projection)
@@ -53,18 +60,21 @@ class simulator(torch.nn.Module):
         return projection.real
 
     def init_volume(self):
-        """Initializes the volume inside the projector with the
-        mrcfile whose path is given in config.input_volume_path
-        if the path is not given or doesn't exist then the volume is intialized with a cube
+        """Initialize the volume inside the projector.
+
+        Initializaes the mrcfile whose path is given in config.input_volume_path.
+        If the path is not given or doesn't exist then the volume
+        is initialized with a cube.
         """
         if (
             self.config.input_volume_path == ""
             or os.path.exists(os.path.join(os.getcwd(), self.config.input_volume_path))
-            == False
+            is False
         ):
 
             print(
-                "No input volume specified or the path doesn't exist. Using cube as the default volume."
+                "No input volume specified or the path doesn't exist. "
+                "Using cube as the default volume."
             )
             volume = init_cube(self.config.sidelen)
         else:
@@ -74,15 +84,22 @@ class simulator(torch.nn.Module):
         self.Projector.vol = volume
 
 
+"""Class to generate tomographic projection."""
+
+
 class Projector(torch.nn.Module):
+    """Class to generate tomographic projection.
+
+    Parameters
+    ----------
+    config: class
+        Class containing parameters of the Projector
+
+    """
+
     def __init__(self, config):
         super(Projector, self).__init__()
-        """modules that generates tomographic projection
-        Parameters
-        ----------
-        config: class
-            Class containing parameters of the Projector
-        """
+
         self.config = config
         self.vol = torch.rand([self.config.sidelen] * 3, dtype=torch.float32)
         lincoords = torch.linspace(
@@ -93,18 +110,23 @@ class Projector(torch.nn.Module):
         self.register_buffer("vol_coords", coords.reshape(-1, 3))
 
     def forward(self, rot_params, proj_axis=-1):
-        """Outputs the tomographic projection of the volume by first
-        rotating it using and then summing it along an axis
+        """Output the tomographic projection of the volume.
+
+        First rotate the volume and then sum it along an axis.
+
         Parameters
         ----------
-            rot_params: dict of type str to {tensor}
-                contains rotation matrix "rotmat" (chunks x 3 x 3) that is used to rotate the volume
-            proj_axis: int
-                index along which summation is done of the rotated volume
+        rot_params: dict of type str to {tensor}
+            contains rotation matrix "rotmat" (chunks x 3 x 3) that is
+            used to rotate the volume
+        proj_axis: int
+            index along which summation is done of the rotated volume
+
         Returns
         -------
-            projection: tensor
-                Tensor containing tomographic projection (chunks x 1 x sidelen x sidelen)
+        projection: tensor
+            Tensor containing tomographic projection
+            (chunks x 1 x sidelen x sidelen)
         """
         rotmat = rot_params["rotmat"]
         batch_sz = rotmat.shape[0]
@@ -129,15 +151,21 @@ class Projector(torch.nn.Module):
         return projection
 
 
+"""Module that multiplies ctf to fourier transform of the projection."""
+
+
 class CTF(torch.nn.Module):
+    """Class that multiplies ctf to fourier transform of the projection.
+
+    Parameters
+    ----------
+    config: class
+        Class containing parameters of the CTF
+
+    """
+
     def __init__(self, config):
         super(CTF, self).__init__()
-        """module that multiplies ctf to fourier transform of the projection
-        Parameters
-        ----------
-            config: class
-                Class containing parameters of the CTF
-        """
 
         self.config = config
         self.wavelength = self._get_ewavelength()
@@ -150,11 +178,12 @@ class CTF(torch.nn.Module):
         self.register_buffer("angleFrequency", torch.atan2(my, mx))
 
     def _get_ewavelength(self):
-        """outputs the wavelength of the electron beam
+        """Output the wavelength of the electron beam.
+
         Returns
-        ------
-            wavelength: float
-                wavelength of the electron beam
+        -------
+        wavelength: float
+            wavelength of the electron beam
         """
         wavelength = 12.2639 / np.sqrt(
             self.config.kV * 1e3 + 0.97845 * self.config.kV ** 2
@@ -162,15 +191,18 @@ class CTF(torch.nn.Module):
         return wavelength
 
     def get_ctf(self, ctf_params):
-        """outputs the CTF for a given params
+        """Output the CTF for a given params.
+
         Parameters
         ----------
-            ctf_params: dict of type str to {tensor}
-                contains "defocusU", "defocusV", "defocusAngle", all of shape (chunks,1,1,1)
+        ctf_params: dict of type str to {tensor}
+            contains "defocusU", "defocusV", "defocusAngle",
+            all of shape (chunks,1,1,1)
+
         Returns
         -------
-            hFourier: torch.Tensor
-                ctf of shape (chunks,1,ctf_size,ctf_size)
+        hFourier: torch.Tensor
+            ctf of shape (chunks,1,ctf_size,ctf_size)
         """
         defocusU = ctf_params["defocusU"]
         defocusV = ctf_params["defocusV"]
@@ -216,18 +248,21 @@ class CTF(torch.nn.Module):
         return hFourier
 
     def forward(self, x_fourier, ctf_params={}):
-        """Multiplies the CTF and projection in fourier domain if ctf_params is not empty,
+        """Multiply the CTF and projection in fourier domain.
+
+        If ctf_params is empty then skip multiplication.
 
         Parameters
         ----------
-            x_fourier: torch.Tensor
-                fourier transform of the projection (chunks,1,sidelen,sidelen)
-            ctf_params: dict of type str to {torch.Tensor}
-                contains parameters of the ctf
+        x_fourier: torch.Tensor
+            fourier transform of the projection (chunks,1,sidelen,sidelen)
+        ctf_params: dict of type str to {torch.Tensor}
+            contains parameters of the ctf
+
         Returns
         -------
-            x_fourier: torch.Tensor
-                modulated fourier transform of the projection (chunks,1,sidelen,sidelen)
+        x_fourier: torch.Tensor
+            modulated fourier transform of the projection (chunks,1,sidelen,sidelen)
         """
         if ctf_params:
             hFourier = self.get_ctf(ctf_params)
@@ -237,17 +272,22 @@ class CTF(torch.nn.Module):
         return x_fourier
 
 
+"""Module containing method to shift an image in Fourier domain."""
+
+
 class Shift(torch.nn.Module):
+    """class containing method to shift an image in Fourier domain.
+
+    Parameters
+    ----------
+    config: class
+        Class containing parameters of the shift
+
+    """
+
     def __init__(self, config):
         super(Shift, self).__init__()
-        """
-           A class containing method to shift an image in Fourier domain.
-           ...
-           Parameters
-           ----------
-           config: class
-                Class containing parameters of the shift
-        """
+
         self.config = config
         self.frequency = 1.0 / (self.config.sidelen * self.config.pixel_size)
 
@@ -261,24 +301,26 @@ class Shift(torch.nn.Module):
         self.register_buffer("my", my.clone())
 
     def modulate(self, x_fourier, t_x, t_y):
-        """
-        outputs modulated (fourier equivalent of shifting in primal domain) input images given in batch wise format.
+        """Output modulated (fourier equivalent of shifting in primal domain).
+
+        Input images given in batch wise format.
         The modulation depends on t_x, t_y.
+
         Parameters
         ----------
-            x_fourier : torch.Tensor
-                batch of input images (Bx1xSizexSize) in Fourier domain
-            t_x: torch.Tensor
-                batch of shifts (B,) along horizontal axis
-            t_y: torch.Tensor
-                batch of shifts (B,) along vertical axis
+        x_fourier : torch.Tensor
+            batch of input images (Bx1xSizexSize) in Fourier domain
+        t_x: torch.Tensor
+            batch of shifts (B,) along horizontal axis
+        t_y: torch.Tensor
+            batch of shifts (B,) along vertical axis
+
         Returns
         -------
-            output: torch.Tensor
-                batch of modulated fourier images (Bx1xSizexSize) given by
-                output(f_1,f_2)=e^{-2*pi*j*[f_1,f_2]*[t_x, t_y] }*input(f_1,f_2)
+        output: torch.Tensor
+            batch of modulated fourier images (Bx1xSizexSize) given by
+            output(f_1,f_2)=e^{-2*pi*j*[f_1,f_2]*[t_x, t_y] }*input(f_1,f_2)
         """
-
         t_y = t_y[:, None, None, None]  # [B,1,1,1]
         t_x = t_x[:, None, None, None]  # [B,1,1,1]
 
@@ -289,23 +331,27 @@ class Shift(torch.nn.Module):
         return x_fourier * modulation  # [B,1,Size,Size]*[B,1,Size,Size]
 
     def forward(self, x_fourier, shift_params={}):
-        """
-        outputs modulated (fourier equivalent of shifting in primal domain) input images given in batch wise format.
-        The modulation depends on t_x, t_y.
+        """Output modulated input images.
+
+        Fourier equivalent of shifting in primal domain. The modulation
+        depends on t_x, t_y.
+
         Parameters
         ----------
-            x_fourier : torch.Tensor
-                batch of input images (Bx1xSizexSize) in Fourier domain
-            shift_params: dict of type str to {torch.Tensor}
-                dictionary containing
-                'shiftX': torch.Tensor (B,)
-                    batch of shifts along horizontal axis
-                'shiftY': torch.Tensor (B,)
-                    batch of shifts along vertical axis
+        x_fourier : torch.Tensor
+            batch of input images (Bx1xSizexSize) in Fourier domain
+        shift_params: dict of type str to {torch.Tensor}
+            dictionary containing
+            'shiftX': torch.Tensor (B,)
+                batch of shifts along horizontal axis
+            'shiftY': torch.Tensor (B,)
+                batch of shifts along vertical axis
+
         Returns
         -------
             output: torch.Tensor
-                batch of modulated fourier images (Bx1xSizexSize) if shift_params is not empty else input is outputted
+                batch of modulated fourier images (Bx1xSizexSize)
+                if shift_params is not empty else input is outputted
         """
         if shift_params:
             x_fourier = self.modulate(
@@ -315,22 +361,37 @@ class Shift(torch.nn.Module):
         return x_fourier
 
 
+"""Module to corrupt the projection with noise."""
+
+
 class Noise(torch.nn.Module):
+    """Class to corrupt the projection with noise.
+
+    Parameters
+    ----------
+    config: class
+        contains parameters of the noise distribution
+
+    """
+
     def __init__(self, config):
-        """corrupts the projection with noise
-        Parameters
-        ----------
-            config: class
-                contains parameters of the noise distribution
-        """
+
         super(Noise, self).__init__()
         self.noise_sigma = config.noise_sigma
 
     def forward(self, proj):
-        """adds noise to proj. Currently, only supports additive white gaussian noise.
+        """Add noise to projections.
+
+        Currently, only supports additive white gaussian noise.
+
         Parameters
-        ---------
-            proj: torch.Tensor
-                input projection of shape B,1,sidelen,sidelen
+        ----------
+        proj: torch.Tensor
+            input projection of shape B,1,sidelen,sidelen
+
+        Returns
+        -------
+        out: torch.Tensor
+            noisy projection of shape B,1,sidelen,sidelen
         """
         return proj + self.noise_sigma * torch.randn_like(proj)
