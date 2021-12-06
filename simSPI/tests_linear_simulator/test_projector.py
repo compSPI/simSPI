381fd39b7de8917c01bd6a4bba410e7693200c5c
Harshit Gupta <hrshit786@gmail.com>
2021-12-06 21:58:33 +0530

    test function and data for it

---
+++ simSPI/tests_linear_simulator/test_projector.py
@@ -0,0 +1,74 @@
+"""Test function for projector module."""
+from ..linear_simulator.projector_utils import Projector
+import numpy as np
+
+class AttrDict(dict):
+    """Class to convert a dictionary to a class.
+
+    Parameters
+    ----------
+    dict: dictionary
+
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Return a class with attributes equal to the input dictionary."""
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+
+def init_data(path):
+    """Load data for the test functions.
+
+    Loads .npy file from a path and converts its config dictionary into a class
+
+    Parameters
+    ----------
+    path: str
+
+    Returns
+    -------
+    saved_data: dict
+        loaded dictionary
+    config: object
+    """
+    saved_data = np.load(path, allow_pickle=True).item()
+    if "config_dict" in saved_data:
+        config_dict = saved_data["config_dict"]
+    else:
+        config_dict = {}
+    config = AttrDict(config_dict)
+    return saved_data, config
+
+
+def normalized_mse(a, b):
+    """Return mean square error.
+
+    Calclulates mean square error between two inputs normalized
+    by the norm of the first input.
+
+    Parameters
+    ----------
+    a: torch.Tensor
+    b: torch.Tensor
+
+    Returns
+    -------
+    out: torch.Tensor
+        normalized mse
+    """
+    return (a - b).pow(2).sum().sqrt() / a.pow(2).sum().sqrt()
+
+
+def test_projector():
+    """Test accuracy of projector function."""
+    path = "simSPI/tests_linear_simulator/data/projector_data.npy"
+
+    saved_data, config = init_data(path)
+    rot_params = saved_data["rot_params"]
+    projector = Projector(config)
+    projector.vol = saved_data["volume"]
+
+    out = projector(rot_params)
+    error = normalized_mse(saved_data["projector_output"], out).item()
+    assert (error < 0.01) == 1
