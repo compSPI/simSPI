125192f7c6b4aa5494cde1e458d7a0c28b25ae04
jed <jed324@gmail.com>
2022-03-09 21:32:28 -0800

    fix imports

--- simSPI/tem.py
+++ simSPI/tem.py
@@ -6,9 +6,9 @@ from pathlib import Path
 
 import numpy as np
 import yaml
-from ioSPI.ioSPI import micrographs
+from ioSPI import micrographs
 
-from simSPI.simSPI import crd, fov, tem_inputs
+from simSPI import crd, fov, tem_inputs
 
 
 class TEMSimulator:
