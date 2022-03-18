8b6b06887901949e1decbe6a146a26b3d6777bce
arjunsingh3600 <swaniarjun@gmail.com>
2022-03-17 20:02:30 -0700

    added tests for tem_distribution

--- tests/test_tem_distribution.py
+++ tests/test_tem_distribution.py
@@ -1,7 +1,7 @@
 """Tests for tem_distribution."""
 import pytest
 import torch
-
+import numpy as np
 from simSPI import tem_distribution
 
 
@@ -17,10 +17,30 @@ def test_constructor():
         tem_distribution.DistributionGenerator("invalid_value", (0, 1))
 
 
-def test_draw_samples_1d():
+def test_draw_samples_uniform():
     """Test if samples of expected length are returned."""
-    generator = tem_distribution.DistributionGenerator("uniform", (0, 10))
 
-    samples = generator.draw_samples_1d(10)
+    upper, lower = (10, 0)
+    size = 100
+    generator = tem_distribution.DistributionGenerator("uniform", [lower, upper])
+
+    samples = generator.draw_samples_1d(size)
     assert type(samples) is torch.Tensor
-    assert samples.shape == torch.Size([10])
+    assert samples.shape == torch.Size([size])
+    assert samples[(samples < lower) & (samples > upper)].shape == torch.Size([0])
+
+
+def test_draw_samples_gaussian():
+    """Test if samples of expected length are returned."""
+    loc, scale = (5,10)
+    size = 10000
+    generator = tem_distribution.DistributionGenerator("gaussian", [loc,scale])
+    samples = generator.draw_samples_1d(size)
+
+    mean_error = 3.69 * scale/np.sqrt(size)
+    std_error = 0.1
+
+    assert type(samples) is torch.Tensor
+    assert samples.shape == torch.Size([size])
+    assert np.abs(loc - torch.mean(samples).item()) < mean_error
+    assert np.abs(scale - torch.std(samples).item()) < std_error
