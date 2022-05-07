ba7210572b6322c3d9ae8c4b08fb530470923a56
Roy <roy.lederman@yale.edu>
2022-05-06 20:00:13 -0400

    scale grids after rotation

--- simSPI/linear_simulator/projector.py
+++ simSPI/linear_simulator/projector.py
@@ -1,7 +1,7 @@
 """Class to generate tomographic projection."""
 
-import torch
 import numpy as np
+import torch
 from pytorch3d.transforms import Rotate
 
 
@@ -23,7 +23,7 @@ class Projector(torch.nn.Module):
 
         self.config = config
         self.space = config.space
-        
+
         if self.space == "real":
             self.vol = torch.rand([self.config.side_len] * 3, dtype=torch.float32)
             lin_coords = torch.linspace(-1.0, 1.0, self.config.side_len)
@@ -34,12 +34,15 @@ class Projector(torch.nn.Module):
                 * 3
             )
             coords = torch.stack([y, x, z], dim=-1)
+            # Rescale coordinates to [-1,1] to be compatible with torch.nn.functional.grid_sample
+            coords = 2 * coords
+
             self.register_buffer("vol_coords", coords.reshape(-1, 3))
         elif self.space == "fourier":
             # Assume DC coefficient is at self.vol[n//2+1,n//2+1]
             # this means that self.vol = fftshift(fft3(fftshift(real_vol)))
             self.vol = torch.rand([self.config.side_len] * 3, dtype=torch.complex64)
-            freq_coords = torch.fft.fftfreq(self.config.side_len,dtype=torch.float32)
+            freq_coords = torch.fft.fftfreq(self.config.side_len, dtype=torch.float32)
             [x, y] = torch.meshgrid(
                 [
                     freq_coords,
@@ -47,11 +50,11 @@ class Projector(torch.nn.Module):
                 * 2
             )
             coords = torch.stack([y, x], dim=-1)
-            self.register_buffer("vol_coords", coords.reshape(-1, 2))            
+            self.register_buffer("vol_coords", coords.reshape(-1, 2))
 
     def forward(self, rot_params, proj_axis=-1):
         if self.space == "real":
-            return self._forward_real(rot_params,proj_axis)
+            return self._forward_real(rot_params, proj_axis)
         elif self.space == "fourier":
             return self._forward_fourier(rot_params)
 
@@ -77,33 +80,30 @@ class Projector(torch.nn.Module):
             Tensor containing tomographic projection
             (batch_size x 1 x sidelen x sidelen)
         """
-        
+
         rotmat = rot_params["rotmat"]
         batch_sz = rotmat.shape[0]
-        rot_vol_coords = 2*self.vol_coords.repeat((batch_sz,1,1)).bmm(rotmat[:,:2,:])
-        
-        projection = torch.empty((batch_sz,
-                                  self.config.side_len,
-                                  self.config.side_len), dtype=torch.complex64)
+        rot_vol_coords = self.vol_coords.repeat((batch_sz, 1, 1)).bmm(rotmat[:, :2, :])
+
+        projection = torch.empty(
+            (batch_sz, self.config.side_len, self.config.side_len),
+            dtype=torch.complex64,
+        )
         projection.real = torch.nn.functional.grid_sample(
             self.vol.real.repeat((batch_sz, 1, 1, 1, 1)),
             rot_vol_coords[:, None, None, :, :],
             align_corners=True,
-        ).reshape(batch_sz,
-                  self.config.side_len,
-                  self.config.side_len)
+        ).reshape(batch_sz, self.config.side_len, self.config.side_len)
 
         projection.imag = torch.nn.functional.grid_sample(
             self.vol.imag.repeat((batch_sz, 1, 1, 1, 1)),
             rot_vol_coords[:, None, None, :, :],
             align_corners=True,
-        ).reshape(batch_sz,
-                  self.config.side_len,
-                  self.config.side_len)
+        ).reshape(batch_sz, self.config.side_len, self.config.side_len)
 
         projection = projection[:, None, :, :]
         return projection
-    
+
     def _forward_real(self, rot_params, proj_axis=-1):
         """Output the tomographic projection of the volume.
 
@@ -131,6 +131,18 @@ class Projector(torch.nn.Module):
         batch_sz = rotmat.shape[0]
         t = Rotate(rotmat, device=self.vol_coords.device)
         rot_vol_coords = t.transform_points(self.vol_coords.repeat(batch_sz, 1, 1))
+
+        # rescale the coordinates to be compatible with the edge alignment of torch.nn.functional.grid_sample
+        if 0 == self.config.side_len % 2:  # even case
+            rot_vol_coords = (
+                (rot_vol_coords + 1)
+                * (self.config.side_len + 1)
+                / (self.config.side_len)
+            ) - 1
+        else:  # odd case
+            rot_vol_coords = (
+                (rot_vol_coords) * (self.config.side_len + 1) / (self.config.side_len)
+            )
 
         rot_vol = torch.nn.functional.grid_sample(
             self.vol.repeat(batch_sz, 1, 1, 1, 1),
