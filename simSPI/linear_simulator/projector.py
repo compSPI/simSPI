062d0dd7dfc49d55239d81eb2c09781dd4ae2bf6
Roy <roy.lederman@yale.edu>
2022-05-10 15:24:35 -0400

    test fft scale

--- simSPI/linear_simulator/projector.py
+++ simSPI/linear_simulator/projector.py
@@ -77,19 +77,30 @@ class Projector(torch.nn.Module):
         Returns
         -------
         projection: tensor
-            Tensor containing tomographic projection
+            Tensor containing tomographic projection in the Fourier domain
             (batch_size x 1 x sidelen x sidelen)
+        
+        Comments
+        --------
+        Note that the Fourier volumes are arbitrary channel x height x width complex valued tensors, 
+        they are not assumed to be Fourier transforms of a real valued 3D functions.
+        
+        Note that the tomographic projection is interpolated on a rotated 2D grid. 
+        The rotated 2D grid extends outside the boundaries of the 3D grid. 
+        The values outside the boundaries are not defined in a useful way. 
+        Therefore, in most applications, it make sense to apply a radial filter to the sample.
+
         """
 
         rotmat = rot_params["rotmat"]
         batch_sz = rotmat.shape[0]
         
-        print(rotmat[0])
+        #print(rotmat[0])
         rotmat = torch.transpose(rotmat,-1,-2)
-        print(rotmat[0])
+        #print(rotmat[0])
         rot_vol_coords = self.vol_coords.repeat((batch_sz, 1, 1)).bmm(rotmat[:, :2, :])
-        print(rot_vol_coords[0])
-        print(rot_vol_coords[1])
+        #print(rot_vol_coords[0])
+        #print(rot_vol_coords[1])
         
         # rescale the coordinates to be compatible with the edge alignment of torch.nn.functional.grid_sample
         if 0 == self.config.side_len % 2:  # even case
@@ -107,16 +118,22 @@ class Projector(torch.nn.Module):
             (batch_sz, self.config.side_len, self.config.side_len),
             dtype=torch.complex64,
         )
+        # interpolation is decomposed to real and imaginary parts due to torch grid_sample type rules. Requires data and coordinates of same type.
+        # padding_mode="reflection" is required due to possible pathologies right on the border.
+        # however, padding_mode="zeros" is what users might expect in most cases other than these axis aligned cases.
+        padding_mode="zeros"
         projection.real = torch.nn.functional.grid_sample(
             self.vol.real.repeat((batch_sz, 1, 1, 1, 1)),
             rot_vol_coords[:, None, None, :, :],
             align_corners=True,
+            padding_mode=padding_mode,
         ).reshape(batch_sz, self.config.side_len, self.config.side_len)
 
         projection.imag = torch.nn.functional.grid_sample(
             self.vol.imag.repeat((batch_sz, 1, 1, 1, 1)),
             rot_vol_coords[:, None, None, :, :],
             align_corners=True,
+            padding_mode=padding_mode,
         ).reshape(batch_sz, self.config.side_len, self.config.side_len)
 
         projection = projection[:, None, :, :]
